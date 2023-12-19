4aa0e6a66869d27cba85b919f137ddd21afa8f0c
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2023-12-19 13:32:16 +0100

    Fix for current Rust.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(extern_crate_item_prelude)]
 #![feature(proc_macro_span)]
 
 //! An alternative [`Display`] impl for [proc_macro], respecting the input layout and formatting.
@@ -27,96 +26,117 @@
 
 extern crate proc_macro;
 
+use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Span, TokenStream, TokenTree};
 use std::fmt::{self, Display, Write};
 use std::iter::FromIterator;
-use proc_macro::{Delimiter, Group, Ident, LineColumn, Literal, Punct, TokenStream, TokenTree};
 
 /// A more faithful [`Display`].
 ///
-/// This trait works by accumulating a [`LineColumn`] as it formats tokens. By recomputing on the
+/// This trait works by accumulating a [`Span`] as it formats tokens. By recomputing on the
 /// fly on the layout of each token, it’s possible to insert newlines and spaces to respect the
 /// initial formatting.
 pub trait FaithfulDisplay {
-  /// Display a token in a faithful way.
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error>;
+    /// Display a token in a faithful way.
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error>;
 }
 
 impl FaithfulDisplay for Ident {
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error> {
-    let current_span = self.span();
-    whitespace_adjust_span(f, prev_span, current_span.start())?;
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error> {
+        let current_span = self.span();
+        whitespace_adjust_span(f, prev_span, current_span.start())?;
 
-    self.fmt(f).map(|_| current_span.end())
-  }
+        self.fmt(f).map(|_| current_span.end())
+    }
 }
 
 impl FaithfulDisplay for Literal {
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error> {
-    let current_span = self.span();
-    whitespace_adjust_span(f, prev_span, current_span.start())?;
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error> {
+        let current_span = self.span();
+        whitespace_adjust_span(f, prev_span, current_span.start())?;
 
-    self.fmt(f).map(|_| current_span.end())
-  }
+        self.fmt(f).map(|_| current_span.end())
+    }
 }
 
 impl FaithfulDisplay for Punct {
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error> {
-    let current_span = self.span();
-    whitespace_adjust_span(f, prev_span, current_span.start())?;
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error> {
+        let current_span = self.span();
+        whitespace_adjust_span(f, prev_span, current_span.start())?;
 
-    f.write_char(self.as_char()).map(|_| current_span.end())
-  }
+        f.write_char(self.as_char()).map(|_| current_span.end())
+    }
 }
 
 impl FaithfulDisplay for Group {
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error> {
-    let current_span = self.span_open();
-    whitespace_adjust_span(f, prev_span, current_span.start())?;
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error> {
+        let current_span = self.span_open();
+        whitespace_adjust_span(f, prev_span, current_span.start())?;
 
-    match self.delimiter() {
-      Delimiter::Parenthesis => {
-        faithful_delimited(f, '(', ')', self.stream(), current_span.end(), self.span_close().start())?;
-      }
+        match self.delimiter() {
+            Delimiter::Parenthesis => {
+                faithful_delimited(
+                    f,
+                    '(',
+                    ')',
+                    self.stream(),
+                    current_span.end(),
+                    self.span_close().start(),
+                )?;
+            }
 
-      Delimiter::Brace => {
-        faithful_delimited(f, '{', '}', self.stream(), current_span.end(), self.span_close().start())?;
-      }
+            Delimiter::Brace => {
+                faithful_delimited(
+                    f,
+                    '{',
+                    '}',
+                    self.stream(),
+                    current_span.end(),
+                    self.span_close().start(),
+                )?;
+            }
 
-      Delimiter::Bracket => {
-        faithful_delimited(f, '[', ']', self.stream(), current_span.end(), self.span_close().start())?;
-      }
+            Delimiter::Bracket => {
+                faithful_delimited(
+                    f,
+                    '[',
+                    ']',
+                    self.stream(),
+                    current_span.end(),
+                    self.span_close().start(),
+                )?;
+            }
 
-      Delimiter::None => {
-        let line_col = self.stream().faithful_fmt(f, current_span.end())?;
-        whitespace_adjust_span(f, prev_span, line_col)?;
-      }
+            Delimiter::None => {
+                let line_col = self.stream().faithful_fmt(f, current_span.end())?;
+                whitespace_adjust_span(f, prev_span, line_col)?;
+            }
+        }
+
+        Ok(self.span_close().end())
     }
-
-    Ok(self.span_close().end())
-  }
 }
 
 impl FaithfulDisplay for TokenStream {
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error> {
-    let mut current_span = prev_span;
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error> {
+        let mut current_span = prev_span;
 
-    for tree in self.clone() {
-      current_span = tree.faithful_fmt(f, current_span)?;
+        for tree in self.clone() {
+            current_span = tree.faithful_fmt(f, current_span)?;
+        }
+
+        Ok(current_span)
     }
-
-    Ok(current_span)
-  }
 }
 
 impl FaithfulDisplay for TokenTree {
-  fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: LineColumn) -> Result<LineColumn, fmt::Error> {
-    match self {
-      TokenTree::Group(gr) => gr.faithful_fmt(f, prev_span),
-      TokenTree::Ident(ident) => ident.faithful_fmt(f, prev_span),
-      TokenTree::Punct(p) => p.faithful_fmt(f, prev_span),
-      TokenTree::Literal(lit) => lit.faithful_fmt(f, prev_span)
+    fn faithful_fmt(&self, f: &mut fmt::Formatter, prev_span: Span) -> Result<Span, fmt::Error> {
+        match self {
+            TokenTree::Group(gr) => gr.faithful_fmt(f, prev_span),
+            TokenTree::Ident(ident) => ident.faithful_fmt(f, prev_span),
+            TokenTree::Punct(p) => p.faithful_fmt(f, prev_span),
+            TokenTree::Literal(lit) => lit.faithful_fmt(f, prev_span),
+        }
     }
-  }
 }
 
 /// Create a [`Display`] object out of a [`TokenStream`] that respects as closely as possible its
@@ -125,27 +145,29 @@ impl FaithfulDisplay for TokenTree {
 /// > Disclaimer: because this function takes a reference and because [`TokenStream`] – at the time
 /// > of writing – doesn’t support reference-based iteration, a complete deep clone of the token
 /// > tree has to be performed prior to displaying it.
-pub fn faithful_display<'a>(stream: &'a TokenStream) -> impl Display + 'a {
-  struct D<'a>(&'a TokenStream);
+pub fn faithful_display(stream: &TokenStream) -> impl Display + '_ {
+    struct D<'a>(&'a TokenStream);
 
-  impl<'a> fmt::Display for D<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-      // get the first span, if any
-      let mut iter = self.0.clone().into_iter();
-      let first = iter.next();
+    impl<'a> fmt::Display for D<'a> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+            // get the first span, if any
+            let mut iter = self.0.clone().into_iter();
+            let first = iter.next();
 
-      if let Some(tree) = first {
-        let first_line_col = tree.span().start();
-        let line_col = tree.faithful_fmt(f, first_line_col)?;
+            if let Some(tree) = first {
+                let first_line_col = tree.span().start();
+                let line_col = tree.faithful_fmt(f, first_line_col)?;
 
-        TokenStream::from_iter(iter).faithful_fmt(f, line_col).map(|_| ())
-      } else {
-        Ok(())
-      }
+                TokenStream::from_iter(iter)
+                    .faithful_fmt(f, line_col)
+                    .map(|_| ())
+            } else {
+                Ok(())
+            }
+        }
     }
-  }
 
-  D(stream)
+    D(stream)
 }
 
 /// Automatically adjust with whitespaces a formatter based on the current span and the previous
@@ -154,36 +176,36 @@ pub fn faithful_display<'a>(stream: &'a TokenStream) -> impl Display + 'a {
 /// This function is key to the overall implementation, has it enables to respect the input
 /// indentation and general formatting.
 fn whitespace_adjust_span(
-  f: &mut fmt::Formatter,
-  prev_span: LineColumn,
-  current_span: LineColumn 
+    f: &mut fmt::Formatter,
+    prev_span: Span,
+    current_span: Span,
 ) -> Result<(), fmt::Error> {
-  if current_span.line == prev_span.line {
-    // we are on the same line, we just have to adjust the number of spaces
-    let nb_spaces = current_span.column - prev_span.column;
-    f.write_str(" ".repeat(nb_spaces).as_str())
-  } else {
-    // we are on different lines; first add the newlines difference, then adjust with spaces
-    let nb_newlines = current_span.line - prev_span.line;
-    let nb_spaces = current_span.column;
-    f.write_str("\n".repeat(nb_newlines).as_str())?;
-    f.write_str(" ".repeat(nb_spaces).as_str())
-  }
+    if current_span.line() == prev_span.line() {
+        // we are on the same line, we just have to adjust the number of spaces
+        let nb_spaces = current_span.column() - prev_span.column();
+        f.write_str(" ".repeat(nb_spaces).as_str())
+    } else {
+        // we are on different lines; first add the newlines difference, then adjust with spaces
+        let nb_newlines = current_span.line() - prev_span.line();
+        let nb_spaces = current_span.column();
+        f.write_str("\n".repeat(nb_newlines).as_str())?;
+        f.write_str(" ".repeat(nb_spaces).as_str())
+    }
 }
 
 /// Display a token stream that is surrounded by two matching characters.
 fn faithful_delimited(
-  f: &mut fmt::Formatter,
-  del_first: char,
-  del_end: char,
-  stream: TokenStream,
-  prev_span: LineColumn,
-  final_span: LineColumn
+    f: &mut fmt::Formatter,
+    del_first: char,
+    del_end: char,
+    stream: TokenStream,
+    prev_span: Span,
+    final_span: Span,
 ) -> Result<(), fmt::Error> {
-  f.write_char(del_first)?;
+    f.write_char(del_first)?;
 
-  let current_span = stream.faithful_fmt(f, prev_span)?;
+    let current_span = stream.faithful_fmt(f, prev_span)?;
 
-  whitespace_adjust_span(f, current_span, final_span)?;
-  f.write_char(del_end)
+    whitespace_adjust_span(f, current_span, final_span)?;
+    f.write_char(del_end)
 }
